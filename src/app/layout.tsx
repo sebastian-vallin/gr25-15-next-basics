ab4293c235f437f6cc66a8db54bdb886910cad7e
Sebastian Vallin <82804648+sebastian-vallin@users.noreply.github.com>
2025-11-10 12:06:04 +0100

    day2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Outfit({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-100 font-sans`}
       >
         {children}
       </body>
