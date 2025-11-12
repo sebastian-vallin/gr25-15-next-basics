784872867aa54e2fff399ccb3c147b03ffbbbb75
Sebastian Vallin <82804648+sebastian-vallin@users.noreply.github.com>
2025-11-12 12:10:28 +0100

    day3

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,59 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-4xl">Home</h1>
+      <h1 className="font-bold text-4xl mb-4">Home</h1>
+
+      {/* <Button size="lg" className="text-lg font-bold" asChild>
+        <Link href="/about">
+          About Us
+          <span>hello</span>
+        </Link>
+      </Button> */}
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Open Dialog</Button>
+        </DialogTrigger>
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+
+          {/* Main Content of Dialog */}
+          <form>
+            <label className="pr-2">Test</label>
+            <input />
+          </form>
+
+          <DialogFooter className="sm:justify-start">
+            <Button variant="destructive">Delete</Button>
+
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Cancel
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
--- src/components/main-navbar.tsx
+++ src/components/main-navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MainNavbar() {
   return (
-    <nav className="p-4 bg-neutral-50 border-b border-b-neutral-300">
+    <nav className="p-4 bg-secondary border-b border-b-border">
       <ul className="flex gap-2">
         <li>
           <Link href="/">Home</Link>
